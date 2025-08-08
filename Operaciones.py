871dab8ff525ee3a7c046bd0e3bce78cb92d04e1
Ana Campos <128433087+Ana-Campos2023@users.noreply.github.com>
2025-08-08 12:50:18 -0600

    Ejercicios de operaciones artimeticas

---
+++ Operaciones.py
@@ -0,0 +1,10 @@
+"""Codigo para efectuar diferentes operaciones aritmeticas mediante diferentes metodos"""
+a = int(input())
+b = int(input())
+print (a+b)
+line2=a-b
+print (line2)
+def line3(x,y):
+    l3=x*y
+    print(l3)
+line3(a,b)
